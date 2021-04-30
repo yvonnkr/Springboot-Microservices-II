d2298bb04a0c35a6ffcdb6621068fcdc2d1269b9
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-04-30 15:09:04 +0100

    use ribbon as load-balancer demo in prod-enquiry-srv

--- product-enquiry-service/src/main/java/com/yvolabs/product/enquiry/service/client/ProductStockClient.java
+++ product-enquiry-service/src/main/java/com/yvolabs/product/enquiry/service/client/ProductStockClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product-stock-service", url="localhost:8800")
+// @FeignClient(name="product-stock-service", url="localhost:8800") (before-ribbon loadbalancer - settings in application.yml)
+@FeignClient(name="product-stock-service")
 public interface ProductStockClient {
 
     @GetMapping("/check-product-stock/productName/{productName}/productAvailability/{productAvailability}")
