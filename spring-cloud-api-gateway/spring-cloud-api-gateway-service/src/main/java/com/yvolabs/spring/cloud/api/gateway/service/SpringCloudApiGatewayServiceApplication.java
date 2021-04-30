693ff3544ea1f13881c5e96f2016431da66ba742
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-04-30 20:22:45 +0100

    added spring-cloud-api-gateway

---
+++ spring-cloud-api-gateway/spring-cloud-api-gateway-service/src/main/java/com/yvolabs/spring/cloud/api/gateway/service/SpringCloudApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yvolabs.spring.cloud.api.gateway.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApiGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-api-gateway/spring-cloud-api-gateway-service/src/test/java/com/yvolabs/spring/cloud/api/gateway/service/SpringCloudApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.spring.cloud.api.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
