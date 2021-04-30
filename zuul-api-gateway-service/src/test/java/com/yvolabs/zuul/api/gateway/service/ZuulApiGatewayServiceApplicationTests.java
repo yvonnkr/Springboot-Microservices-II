bf9fb47a45305961ee91e54791ceaa606f41a091
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-04-30 19:48:41 +0100

    added demo purpose zuul-api-gateway-service

---
+++ zuul-api-gateway-service/src/test/java/com/yvolabs/zuul/api/gateway/service/ZuulApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.zuul.api.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
